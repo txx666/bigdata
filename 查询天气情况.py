a7907f00c32d96107fd7b207d3650a1c3b3f4d1c
txx666 <39868154+txx666@users.noreply.github.com>
2018-06-03 11:53:37 +0800

    Add files via upload
    作业，查询城市天气

--- 查询天气情况.py
+++ 查询天气情况.py
@@ -5,15 +5,35 @@ Created on Sun Jun  3 08:48:06 2018
 @author: lenovo
 """
 
+
+
+
+print("欢迎使用全球天气app")
+
 import urllib.request as r
-url='http://api.openweathermap.org/data/2.5/weather?q=beijing&mode=xml&units=metric&lang=zh_cn&APPID=6aca3ba66b44ae7c4c78b3cedd7dc84e'
-info=r.urlopen(url.format('leshan')).read().decode('utf-8','ingnore')
+city=input("请输入城市：")
+address='http://api.openweathermap.org/data/2.5/forecast?q=zhengzhou,cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996&units=metric'
+info=r.urlopen(address.format(city)).read().decode('utf-8','ignore')
 import json
-w=json.loads(info)
+data=json.loads(info)
 
 
+for i in range(4):
+    b=7*i-6
+    a={"时间":data['list'][b]['dt_txt'],
+   "天气情况":data['list'][b]['weather'][0]['description'],
+   "最高温度":data['list'][b]['main']['temp_max'],
+   "最低温度":data['list'][b]['main']['temp_min']}
 
+    print("\n当前时间为："+a["时间"],
+      "\n天气状况："+a["天气情况"],
+      "\n最高温度为:"+str(a["最高温度"]),
+      "\n最低温度为："+str(a["最低温度"]))
 
+      
+     
+   
+      
 
 
 
